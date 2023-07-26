83a67d6bd984022d5dc30bf4b59cf3bea43721ad
André <AndreR@ppedv.de>
2023-07-26 16:16:06 +0200

    EfContext

---
+++ ppedv.CheesyDrive/ppedv.CheesyDrive.Data.EfCore/EfContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ppedv.CheesyDrive.Model;
+
+namespace ppedv.CheesyDrive.Data.EfCore
+{
+    public class EfContext : DbContext
+    {
+        private readonly string conString;
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Rent> Rents { get; set; }
+
+        //#if net461
+        //public EfContext(string conString) : base(conString)
+        //{
+
+        //}
+        //#endif
+
+        public EfContext(string conString)
+        {
+            this.conString = conString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(conString).UseLazyLoadingProxies();
+        }
+    }
+}
