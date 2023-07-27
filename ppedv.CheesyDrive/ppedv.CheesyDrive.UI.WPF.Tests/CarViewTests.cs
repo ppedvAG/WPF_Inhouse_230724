4e4ad0f43609e2734847c4fea0cdba2212c3e027
André <AndreR@ppedv.de>
2023-07-27 17:28:37 +0200

    Fast mit UI Tests

---
+++ ppedv.CheesyDrive/ppedv.CheesyDrive.UI.WPF.Tests/CarViewTests.cs
@@ -0,0 +1,42 @@
+﻿using FlaUI.Core.AutomationElements;
+using FlaUI.UIA3;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading;
+using FluentAssertions;
+
+namespace ppedv.CheesyDrive.UI.WPF.Tests
+{
+    [TestClass]
+    public class CarViewTests
+    {
+        readonly string appPath = @"C:\Users\ar2\source\repos\WPF_Inhouse_230724\ppedv.CheesyDrive\ppedv.CheesyDrive.UI.WPF\bin\Debug\net48\ppedv.CheesyDrive.UI.WPF.exe";// typeof(MainWindow).Assembly.Location;//.Replace(".dll", ".exe");
+
+
+        FlaUI.Core.Application app;
+        UIA3Automation automation;
+        Window win;
+
+        [TestMethod]
+        [TestCategory( "UITest")]
+        public void Start_app_should_display_some_Cars()
+        {
+            app = FlaUI.Core.Application.Launch(appPath);
+            app.WaitWhileMainHandleIsMissing();
+            automation = new UIA3Automation();
+            win = app.GetMainWindow(automation);
+            win.WaitUntilClickable();
+            //win.WaitUntilEnabled();
+
+            //win.FindFirstDescendant("mp").WaitUntilClickable();
+            //var loadBtn = win.FindFirstDescendant("b1").AsButton();
+            //loadBtn.Click();
+
+            //Thread.Sleep(100);
+            var grid = win.FindFirstDescendant(x => x.ByAutomationId("grid1")).AsDataGridView();
+
+            grid.Rows.Length.Should().BeGreaterThan(1);
+        }
+
+    }
+}
---
+++ ppedv.CheesyDrive/ppedv.CheesyDrive.UI.WPF.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ppedv.CheesyDrive.UI.WPF.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ppedv.CheesyDrive.UI.WPF.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("370a4b18-ccb3-4f05-82e9-dbe08271467f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
